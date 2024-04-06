b028eaa1c36cbd75ee005a81c2782f0dfe5f5af9
Haxintosh <92402488+Haxintosh@users.noreply.github.com>
2024-04-06 02:30:55 +0000

    peak

--- main.js
+++ main.js
@@ -1,43 +1,85 @@
 import './style.css'
 import * as THREE from 'three';
+import { ParallelMeshBVHWorker } from 'three-mesh-bvh/src/workers/ParallelMeshBVHWorker.js';
 import { WebGLPathTracer } from './pathTracer/core/WebGLPathTracer.js';
-// Create a scene
+
 const scene = new THREE.Scene();
-
-// Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// Create a renderer
 const renderer = new THREE.WebGLRenderer();
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-let pathtracer = new webGLPathTracer(renderer);
-pathtracer.updateScene(scene, camera);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
 
-// Add a cube to the scene
-const geometry = new THREE.PlaneGeometry(10, 10);
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-const plane = new THREE.Mesh(geometry, material);
-scene.add(plane);
+const pathTracer = new WebGLPathTracer(renderer);
+pathTracer.tiles.set(2);
+pathTracer.renderScale = Math.max(1 / window.devicePixelRatio, 0.5);
+pathTracer.setBVHWorker(new ParallelMeshBVHWorker());
 
-// Sphere
-const sphereGeo = new THREE.IcosahedronGeometry(1, 10);
-const material2 = new THREE.MeshStandardMaterial( { color: 0xffff00 } ); 
-const ico = new THREE.Mesh( sphereGeo, material2 );
-scene.add(ico);
+await pathTracer.setSceneAsync( scene, camera, {
+  onProgress: v => console.log(v),
+} );
 
-// Animation loop
+animate();
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  // renderer.render(scene, camera);
-  pathtracer.renderSample();
+  pathTracer.renderSample();
 }
-animate();
+
+
+
+// // Create a scene
+// const scene = new THREE.Scene();
+
+// // Create a camera
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+// camera.position.z = 5;
+// camera.rotateX(-0.25);
+
+// // Create a renderer
+// const renderer = new THREE.WebGLRenderer();
+// renderer.toneMapping = THREE.ACESFilmicToneMapping;
+
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// let pathtracer = new WebGLPathTracer(renderer);
+// pathtracer.renderToCanvas = true;
+// pathtracer.setScene(scene, camera);
+// const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+// dirLight.position.set(5, 5, 5);
+// scene.add(dirLight);
+
+// pathtracer.minSamples = 0;
+// pathtracer.setBVHWorker(new ParallelMeshBVHWorker());
+
+// // Add a cube to the scene
+// const geometry = new THREE.PlaneGeometry(10, 10);
+// const material = new THREE.MeshStandardMaterial({ color: 0x00ff00, side: THREE.DoubleSide});
+// const plane = new THREE.Mesh(geometry, material);
+// scene.add(plane);
+
+// // Sphere
+// const sphereGeo = new THREE.IcosahedronGeometry(1, 1);
+// const material2 = new THREE.MeshStandardMaterial( { color: 0xffff00 } ); 
+// const ico = new THREE.Mesh( sphereGeo, material2 );
+// scene.add(ico);
+
+// // Animation loop
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   // renderer.render(scene, camera);
+//   // while (pathtracer.samples < 1)
+  
+//   pathtracer.renderSample();
+
+//   console.log(pathtracer.samples);
+
+//   // if (pathtracer.samples > 9)
+//   // {
+//   //   pathtracer.reset();
+//   // }
+  
+// }
+// animate();
